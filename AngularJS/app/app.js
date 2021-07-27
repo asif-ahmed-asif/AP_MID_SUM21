0b50c6d1cb025a2491db6414cb883ab5be9e751a
Asif <asifahmed1132@gmail.com>
2021-07-28 02:21:03 +0600

    Assignment Done

---
+++ AngularJS/app/app.js
@@ -0,0 +1,49 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(["$routeProvider","$locationProvider",function($routeProvider,$locationProvider) {
+
+    $routeProvider
+    .when("/", {
+        templateUrl : "views/pages/demopage.html"
+    })
+    .when("/demo", {
+        templateUrl : "views/pages/demopage.html",
+        controller: 'demo'
+    })
+    .when("/demo2", {
+        templateUrl : "views/pages/demo2.html",
+          controller: 'demo2'
+    })
+    .when("/products", {
+        templateUrl : "views/pages/products.html",
+        controller: 'products'
+    })
+    .when("/page1", {
+        templateUrl : "views/pages/page1.html",
+        controller: 'products'
+    })
+    .when("/page2", {
+        templateUrl : "views/pages/page2.html",
+        controller: 'products'
+    })
+    .when("/page3", {
+        templateUrl : "views/pages/page3.html",
+        controller: 'products'
+    })
+    .when("/page4", {
+        templateUrl : "views/pages/page4.html",
+        controller: 'products'
+    })
+    .when("/page5", {
+        templateUrl : "views/pages/page5.html",
+        controller: 'products'
+    })
+    .otherwise({
+        redirectTo:"/"
+    });
+      //$locationProvider.html5Mode(true);
+      //$locationProvider.hashPrefix('');
+      //if(window.history && window.history.pushState){
+      //$locationProvider.html5Mode(true);
+  //}
+
+}]);
